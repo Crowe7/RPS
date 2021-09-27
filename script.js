6eb18cafed1dcc72a4f0d72bb42c2c0745a5d890
Crowe7 <jacobocrowe@gmail.com>
2021-09-26 19:34:01 -0500

    Added prompt for player and converted player choice to int for comparison with computer.

--- script.js
+++ script.js
@@ -4,9 +4,28 @@
 // write a third functioin that keeps score of a first of 5 set 
 //maybe use ifs in the third function that ends if either score hits 5 points
 // print set winner
+
+//randomly generate 1-3 for computer play
 function computerPlay() {
 return Math.floor((Math.random() * 3) + 1);
 }
+// get player choice that we will convert to a int to compare with computer
+let getChoice = prompt("Enter: R, P, or S to play!").toLowerCase();
+    if(getChoice === "r"){
+        getChoice = 1;
+        console.log(getChoice);
+    }
+    else{
+        alert("Enter only R,P, or S!");
+    }
+
+  function convertChoice(){
+      if(getChoice === "r"){
+          getChoice.charCodeAt(0);
+      }
+      else if(getChoice === "p"){
+          
+      }
+  }
 
 console.log("Hello Person!");
-console.log (computerPlay())
