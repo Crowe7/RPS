fcd7e69bc2604f8135e6c3240c5b8b81d3cc5caa
Crowe7 <jacobocrowe@gmail.com>
2021-09-27 01:28:56 -0500

    fixed some bugs with computerPlay and getChoice, made a new function that isnt quite finished with playSet

--- script.js
+++ script.js
@@ -6,26 +6,74 @@
 // print set winner
 
 //randomly generate 1-3 for computer play
-function computerPlay() {
-return Math.floor((Math.random() * 3) + 1);
-}
+
+
+
 // get player choice that we will convert to a int to compare with computer
-let getChoice = prompt("Enter: R, P, or S to play!").toLowerCase();
-    if(getChoice === "r"){
-        getChoice = 1;
-    }
-    else if(getChoice === "p"){
-        getChoice = 2;
-    }
-    else if(getChoice === "s"){
-        getChoice =3;
-    }
-    else{
-        alert("Enter only R,P, or S!");
-    }
+let cpuGame = 0;
+let playerGame = 0;
+let getChoice = prompt("Enter: Rock, Paper, or Scissors to play!").toLowerCase();
+const playerChoice = playerPlay();
+const computerChoice = computerPlay(); 
+
+function playerPlay() {
+        if(getChoice === "rock"){
+            console.log(`You used ${getChoice}!`);
+            getChoice = 1;
+        }
+        else if(getChoice === "paper"){
+            console.log(`You used ${getChoice}!`);
+            getChoice = 2;
+        }
+        else if(getChoice === "scissors"){
+            console.log(`You used ${getChoice}!`);
+            getChoice = 3;
+        }
+        else{
+            alert("Enter only Rock, Paper, or Scissors!");
+        }
+}
+function computerPlay() {
+    return Math.floor((Math.random() * 3) + 1);
+}
+         if(computerChoice === 1){
+            console.log("Computer used rock!");
+        }
+        else if(computerChoice === 2){
+            console.log("Computer used paper!");
+        }
+        else if(computerChoice === 3){
+            console.log("Computer used scissors!");
+        }
+        else{
+            console.log("error");
+            console.log(computerChoice);
+        }
+    
 
 
-const playerChoice = getChoice;
-const computerChoice = computerPlay();
+    function playSet(getChoice, computerChoice){
+        if (getChoice === computerChoice){
+            console.log("Tie!");
+        }
+        else if(getChoice === 1 && computerChoice === 2){
+            cpuGame++
+            console.log("Computer wins this round!");
+        }
+        else if(getChoice === 2 && computerChoice === 3){
+            cpuGame++
+            console.log("Computer wins this round!");
+        }
+        else if(getChoice === 3 && computerChoice === 1){
+            cpuGame++
+            console.log("Computer wins this round!");
+        }
+        else{
+            playerGame++
+            console.log("You win this round!");
 
+        }
+    }
+playSet(getChoice, computerChoice);
 console.log("Hello Person!");
+
